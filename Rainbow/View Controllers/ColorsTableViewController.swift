a838010fc28d265e2ca4d07cdb459b64fdb3bcf6
Gerardo Hernandez <gahernandez02@gmail.com>
2019-11-13 13:14:54 -0800

    Set up tableview with colors

--- Rainbow/View Controllers/ColorsTableViewController.swift
+++ Rainbow/View Controllers/ColorsTableViewController.swift
@@ -9,39 +9,38 @@
 import UIKit
 
 class ColorsTableViewController: UITableViewController {
-
+    
+    var colors: [MyColor] = [MyColor(name: "Red", color: .red),
+                             MyColor(name: "Orange", color: .orange),
+                             MyColor(name: "Yellow", color: .yellow),
+                             MyColor(name: "Blue", color: .blue),
+                             MyColor(name: "Cyan", color: .cyan),
+                             MyColor(name: "Purple", color: .purple)]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
     }
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+//cannot create a table without these two funcs
+// these are the 2 required Data Source Methods!! Must have!!
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
+    
+        return colors.count
+    }                                                               // (section, row)
+                                                                    // in this instance the max is (0, 7)
+    // What data is in each cell                                    //address to section and row number (0, 1)
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell", for: indexPath)
+        //creating a dynamic color. using subscipt syntax to get the names of colors above.
+        //indexPath iterates the color array for us.
+        let color = colors[indexPath.row]
+        cell.textLabel?.text = color.name //calling the variable color which is of type MyColor hence .name
 
         return cell
     }
-    */
-
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
