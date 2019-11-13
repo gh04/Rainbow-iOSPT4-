6b0aaf49375546dd71a4f9d223b845750e2bcee3
Gerardo Hernandez <gahernandez02@gmail.com>
2019-11-13 12:18:00 -0800

    Set up initial views

---
+++ Rainbow/Storyboards/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  Rainbow
+//
+//  Created by Gerardo Hernandez on 11/13/19.
+//  Copyright © 2019 Gerardo Hernandez. All rights reserved.
+//
+
+import UIKit
+
+
+struct MyColor {
+    let name: String
+    let color: UIColor
+}
